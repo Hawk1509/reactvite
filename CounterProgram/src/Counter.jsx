00298af60a7c957dc32f2dd8f726b6667762e077
Hawk1509 <jopaulareeckal@gmail.com>
2024-05-12 08:21:25 +0530

    counter app updated

--- CounterProgram/src/Counter.jsx
+++ CounterProgram/src/Counter.jsx
@@ -1,25 +1,45 @@
-import React, {useState} from "react"
+import {useState} from "react"
 
 function Counter(){
     const[count,setCount] = useState(0);
+    const[ChangeNum,setChangeNum] = useState(1);
 
     const increment = () => {
-        setCount(count + 1);
+        setCount(count + Number(ChangeNum));
     }
     
     const decrement = () => {
-        setCount(count - 1);
+        setCount(count - Number(ChangeNum));
     }
 
     const reset = () => {
         setCount(0);
     }
 
+    const textReader= (event) => {
+        setChangeNum (event.target.value);
+
+        /*if (!isNaN(value)){
+            setChangeNum(value);
+        }
+        else{
+            setChangeNum(1);
+        }*/
+    };
+    
+
     return(<div className = "count-container">
             <p className="count-display">{count}</p>
             <button className="counter-button" onClick = {decrement}>Decrement</button>
             <button className="counter-button" onClick={reset}>Reset</button>
             <button className="counter-button" onClick={increment}>Increment</button>
+            <div className="number-reader" onChange={textReader}>
+                <h3>Enter value to increment the number by:</h3>
+                <input 
+                    type="text"
+                    
+                />
+            </div> 
             </div>);
 
 
